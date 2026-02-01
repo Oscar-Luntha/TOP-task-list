1d27d5b22604fda7d26dc56d12707a19443ff73b
Oscar Luntha <lunthaoscar@gmail.com>
2026-02-01 21:59:24 +0200

    create service functions for adding and deleting projects

--- src/services/projectServices.js
+++ src/services/projectServices.js
@@ -1,4 +1,25 @@
 import Project from "../models/project";
+import * as storage from '../data/storage'
+export function getProjects(){
+    return storage.getProjects()
+}
 export function addProject(projectData){
-    return new Project(projectData)
+    if(!projectData.title || !projectData.category){
+        throw new Error("Project Title and category required")
+    }
+
+    const projects = storage.getProjects();
+    const projectExists = projects.some(
+        p => p.title.toLowerCase() === projectData.title.toLowerCase()
+    )
+    if(projectExists){
+        throw new Error('Project already exists')
+    }
+    const newProject = new Project(projectData)
+    storage.saveall([...projects, newProject]);
+    return newProject;
+}
+export function deleteProject(id){
+    const projects = storage.getProjects();
+    storage.saveall(projects.filter(p => p.id !== id))
 }
