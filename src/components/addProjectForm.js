e793f3799fb1a7ba24cbe23fe1a96a3ae194d2fc
Oscar Luntha <lunthaoscar@gmail.com>
2026-02-01 21:56:28 +0200

    change how the component receives form data

--- src/components/addProjectForm.js
+++ src/components/addProjectForm.js
@@ -1,3 +1,5 @@
+import { addProject , getProjects} from "../services/projectServices"
+import { showError, renderProject, renderProjects } from "./helpers"
 export function initAddProjectDialog(){
     const dialog = document.getElementById("addProjectDialog")
     const openBtn = document.getElementById("openAddProject")
@@ -5,16 +7,21 @@ export function initAddProjectDialog(){
     openBtn.addEventListener('click', () => {
         dialog.showModal();
     })
-    form.addEventListener('submit', () => {
-        const formData = new FormData(form)
-        const projectData = {
-            name: formData.get("name"),
-            category : formData.get("category")
+    form.addEventListener('submit',(e) => {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData (form))
+        console.log(data)
+        try{
+            const newProject = addProject(data);
+            renderProject(newProject)
+            form.reset()
+            dialog.close();
+        }catch(err){
+            showError(err.message)
         }
-        form.reset()
-        dialog.close()
     })
     form.addEventListener('reset', () => {
         dialog.close()
     })  
+    renderProjects(getProjects())
 }
