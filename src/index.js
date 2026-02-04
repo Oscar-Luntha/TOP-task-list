637614f46b24ffb72093254fffd3c8c8ad7fc6ad
Oscar Luntha <lunthaoscar@gmail.com>
2026-02-04 21:35:37 +0200

    Initialize add task dialog in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./styles.css";
 import { initAddProjectDialog } from "./components/addProjectForm";
+import { initAddTaskDialog } from "./components/addTaskForm";
 initAddProjectDialog();
+initAddTaskDialog();
